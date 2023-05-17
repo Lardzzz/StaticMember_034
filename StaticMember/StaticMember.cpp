b48bbbba063d4c4ae35ba459994fad220c7b142f
Lardzzz <aku.ikan04@gmail.com>
2023-05-17 18:40:53 +0700

    membuat kelas mahasiswa dengan 7 member, objek nim serta fungsi(method) setNim dan getNim bersifat static

--- StaticMember/StaticMember.cpp
+++ StaticMember/StaticMember.cpp
@@ -20,12 +20,23 @@ void mahasiswa::setID() {
 }
 
 void mahasiswa::printAll() {
-	cout << "ID		= " << id << endl;
+	cout << "ID	= " << id << endl;
 	cout << "Nama	= " << nama << endl;
 	cout << "NIM	= " << nim << endl;
 	cout << endl;
 }
 
 int main() {
-	mahasiswa mhs1()
+	mahasiswa mhs1("Lia Kurnia");
+	mahasiswa mhs2("Asroni");
+	mhs2.nim = 10;
+	mahasiswa mhs3("Andi Kurniawan");
+	mhs3.nim = 20;
+	mahasiswa mhs4("Joko Purbo");
+
+	mhs1.printAll();
+	mhs2.printAll();
+	mhs3.printAll();
+	mhs4.printAll();
+	return 0;
 }
