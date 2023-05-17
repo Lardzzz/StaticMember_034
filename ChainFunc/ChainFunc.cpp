77de37eb4eb12050eba80b5d0607c57afa6fde72
Lardzzz <aku.ikan04@gmail.com>
2023-05-17 19:18:05 +0700

    memanggil chain function

--- ChainFunc/ChainFunc.cpp
+++ ChainFunc/ChainFunc.cpp
@@ -15,5 +15,9 @@ public:
     }
 } bukunya;
 
-int main()
-{
+int main() {
+    bukunya.setJudul("Matematika");
+    cout << bukunya.getJudul();
+    cout << bukunya.setJudul("Matematika").getJudul(); // chain function calls
+    return 0;
+}
